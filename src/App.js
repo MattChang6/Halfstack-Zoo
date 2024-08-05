525753736dc9f85a6ec8201074b883359bfaaba2
NDelfinRuiz <161775313+NDelfinRuiz@users.noreply.github.com>
2024-08-05 15:53:16 -0700

    Update App.js
    Added Cart Page

--- src/App.js
+++ src/App.js
@@ -13,47 +13,49 @@ import { Bat } from './pages/Kids-Bat';
 import { Cheetah } from './pages/Kids-Cheetah';
 import { Chameleon } from './pages/Kids-Chameleon';
 import Footer from './components/Footer';
+import { Cart } from './pages/Cart';
 import './App.css';
-
 
 function App() {
 	return (
-		// always have the nav bar at TOP and BOTTOM(need to code) loaded at all times
 		<>
-		<div>
-			<Link to="/"><h3>M&N Zoo</h3></Link>
-			<nav className="nav justify-content-end">
-				<Link to="/">Home</Link>&emsp;
-				<Link to="/Tickets">Tickets</Link>&emsp;
-				<Link to="/Events">Events</Link>&emsp;
-				<Link to="/Donations">Donations</Link>&emsp;
-				<Link to="/Membership">Membership</Link>&emsp;
-				<Link to="/Mission">Mission</Link>&emsp;
-				<Link to="/Kids">Kids</Link>&emsp;
-				<Link to="/Socials">Socials</Link>&emsp;
-				<Link to="/Shop">Shop</Link>
-			</nav>
-		</div>
+			<div>
+				<Link to="/"><h3>M&N Zoo</h3></Link>
+				<nav className="nav justify-content-end">
+					<Link to="/">Home</Link>&emsp;
+					<Link to="/Tickets">Tickets</Link>&emsp;
+					<Link to="/Events">Events</Link>&emsp;
+					<Link to="/Donations">Donations</Link>&emsp;
+					<Link to="/Membership">Membership</Link>&emsp;
+					<Link to="/Mission">Mission</Link>&emsp;
+					<Link to="/Kids">Kids</Link>&emsp;
+					<Link to="/Socials">Socials</Link>&emsp;
+					<Link to="/Shop">Shop</Link>&emsp;
+					<Link to="/Cart">Cart</Link>
+				</nav>
+			</div>
 
-		<Routes>
-			<Route path="/" element={<Home />} /> 
-			<Route path="/Kids" element={<Kids />}/>
-			<Route path="/Mission" element={<Mission />}/>
-			<Route path="/Tickets" element={<Tickets />}/>
-			<Route path="/Events" element={<Events />}/>
-			<Route path="/Socials" element={<Socials />}/>
-			<Route path="/Donations" element={<Donations />}/>
-			<Route path="/Membership" element={<Membership />}/>
-			<Route path="/Shop" element={<Shop />}/>
-			<Route path="/Kids-Bat" element={<Bat />}/>
-			<Route path="/Kids-Cheetah" element={<Cheetah />}/>
-			<Route path="/Kids-Chameleon" element={<Chameleon />}/>
-		</Routes>
-		<br />
-		<br />
-		<Footer/>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/Kids" element={<Kids />} />
+				<Route path="/Mission" element={<Mission />} />
+				<Route path="/Tickets" element={<Tickets />} />
+				<Route path="/Events" element={<Events />} />
+				<Route path="/Socials" element={<Socials />} />
+				<Route path="/Donations" element={<Donations />} />
+				<Route path="/Membership" element={<Membership />} />
+				<Route path="/Shop" element={<Shop />} />
+				<Route path="/Cart" element={<Cart />} />
+				<Route path="/Kids-Bat" element={<Bat />} />
+				<Route path="/Kids-Cheetah" element={<Cheetah />} />
+				<Route path="/Kids-Chameleon" element={<Chameleon />} />
+			</Routes>
+
+			<br />
+			<br />
+			<Footer />
 		</>
-	)
+	);
 }
 
 export default App;
