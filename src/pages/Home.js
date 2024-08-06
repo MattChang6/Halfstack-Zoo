657a3937b34d1f33e67206330ac853e2033b28fd
MattChang6 <122321464+MattChang6@users.noreply.github.com>
2024-08-05 22:24:31 -0700

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,33 +31,21 @@ export function Home() {
           slideInterval={5000}
         />
         <div>
-          <h3 style={{ 
-            position: 'absolute', 
-            top: '10%', 
-            left: '50%', 
-            transform: 'translate(-50%, -50%)', 
-            color: 'white' 
-          }}>Plan your visit</h3>
-          <h1 style={{ 
-            position: 'absolute', 
-            top: '20%', 
-            left: '50%', 
-            transform: 'translate(-50%, -50%)', 
-            color: 'yellow' 
-          }}>Wild fun awaits!</h1>
+          <h3 className='planVisit'>Plan your visit</h3>
+          <h1 className='wildFun'>Wild fun awaits!</h1>
         </div>
       </div>
       <div style={{ textAlign: 'center', marginTop: '20px' }}>
         <h6>Explore!</h6>
-        <div>
-          <a href="#/Kids" style={{ margin: '0 10px' }}>Kids Corner</a>
-          <a href="#/Shop" style={{ margin: '0 10px' }}>Shop</a>
-          <a href="#/Events" style={{ margin: '0 10px' }}>Panda Extravaganza</a>
+        <div> {/* Place links within cards */}
+          <a href="#/Kids" className='links'>Kids Corner</a>
+          <a href="#/Shop" className='links'>Shop</a>
+          <a href="#/Events" className='links'>Panda Extravaganza</a>
         </div>
         <div>
-          <a href="#/Events" style={{ margin: '0 10px' }}>Summer Parade</a>
-          <a href="#/Tours" style={{ margin: '0 10px' }}>Tickets</a>
-          <a href="#/Mission" style={{ margin: '0 10px' }}>Our Mission</a>
+          <a href="#/Events" className='links'>Summer Parade</a>
+          <a href="#/Tours" className='links'>Tickets</a>
+          <a href="#/Mission" className='links'>Our Mission</a>
         </div>
       </div>
     </div>
